e4fc2663d2246b85668b0a6c1a894624473d92f0
apichat <apichat.kop@gmail.com>
2020-02-06 21:09:37 +0700

    First commit

---
+++ src/test/java/com/linebot/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linebot.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
