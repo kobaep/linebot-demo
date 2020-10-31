57028cc81a6f2ec764351596b97c9d52c5593dd4
wutthinun <godofcyber666@gmail.com>
2020-10-31 18:18:45 +0700

    Change response to json

--- src/main/java/com/linebot/demo/DemoApplication.java
+++ src/main/java/com/linebot/demo/DemoApplication.java
@@ -2,8 +2,12 @@ package com.linebot.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @SpringBootApplication
@@ -14,7 +18,9 @@ public class DemoApplication {
 	}
 
 	@GetMapping()
-	public String incoming() {
-		return "Hello Line bot";
+	public ResponseEntity<?> incoming() {
+		Map<String, Object> r = new HashMap<>();
+		r.put("msg", "Hello Line bot");
+		return ResponseEntity.ok(r);
 	}
 }
